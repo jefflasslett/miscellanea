0508fc3e5a41d68be3430c5dc4cf5a3f4176f76a
Jeff Lasslett <jeff@julietlima.net>
2014-02-24 07:52:43 +1100

    Use avoidStruts to keep xmobar visible

--- xmonad.hs
+++ xmonad.hs
@@ -1,9 +1,11 @@
 import XMonad
-
+import XMonad.Hooks.ManageDocks
 
 myConfig =
-  defaultConfig { terminal = "sakura" }
-               
+  defaultConfig { terminal = "sakura"
+                , manageHook = manageDocks <+> manageHook defaultConfig
+                , layoutHook = avoidStruts $ layoutHook defaultConfig
+                }
 
 main = xmonad myConfig
 
