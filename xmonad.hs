f53b587d7e9fdfefdf7864cf542be38614443724
Jeff Lasslett <jeff@julietlima.net>
2014-07-21 09:31:48 +1000

    Add hotkeys for launching firefox & t-bird

--- xmonad.hs
+++ xmonad.hs
@@ -1,11 +1,17 @@
 import XMonad
 import XMonad.Hooks.ManageDocks
+import XMonad.Util.EZConfig( additionalKeys )
 
 myConfig =
   defaultConfig { terminal = "sakura"
                 , manageHook = manageDocks <+> manageHook defaultConfig
                 , layoutHook = avoidStruts $ layoutHook defaultConfig
-                }
+                } `additionalKeys`
+                [ ( ( mod1Mask .|. controlMask, xK_m ), spawn "synclient TouchpadOff=0" )
+                , ( ( mod1Mask .|. controlMask .|. shiftMask, xK_m ), spawn "synclient TouchpadOff=1" )
+                , ( ( mod1Mask .|. controlMask, xK_t ), spawn "thunderbird" )
+                , ( ( mod1Mask .|. controlMask, xK_f ), spawn "firefox" )
+                ]
 
 main = xmonad myConfig
 
