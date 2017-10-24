744437746d808dfbac9eebe357ca541e9dae476f
Jeff Lasslett <jeffl@ibsglobalweb.com>
2017-10-25 10:23:53 +1100

    Start slock as auto screen locker

--- xmonad.hs
+++ xmonad.hs
@@ -9,14 +9,17 @@ myManageHook = composeAll
 
 myConfig =
   docks $ def { terminal = "urxvt"
+              , manageHook = myManageHook <+> manageHook def
               , layoutHook = avoidStruts $ layoutHook def
+              , handleEventHook = docksEventHook <+> handleEventHook def
               } `additionalKeys`
               [ ( ( mod1Mask .|. controlMask, xK_m ), spawn "synclient TouchpadOff=0" )
               , ( ( mod1Mask .|. controlMask .|. shiftMask, xK_m ), spawn "synclient TouchpadOff=1" )
               , ( ( mod1Mask .|. shiftMask, xK_4 ), spawn "shutter -s" )
               , ( ( mod1Mask .|. controlMask, xK_t ), spawn "thunderbird" )
               , ( ( mod1Mask .|. controlMask, xK_f ), spawn "firefox" )
+              , ( ( mod1Mask .|. controlMask, xK_l ), spawn "slock" )
               ]
 
-main = xmonad myConfig
+main = xmonad $ docks myConfig
 
